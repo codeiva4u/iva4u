4aa9b5a6ecdea545fb12dacecafbd5bf2983cee0
codeiva4u <153196182+codeiva4u@users.noreply.github.com>
2024-11-19 18:58:52 +0530

    Update YomoviesProvider.kt

--- YomviesProvider/src/main/kotlin/com/yomovies/YomoviesProvider.kt
+++ YomviesProvider/src/main/kotlin/com/yomovies/YomoviesProvider.kt
@@ -10,7 +10,7 @@ import org.jsoup.nodes.Element
 import java.net.URI
 
 open class YomoviesProvider : MainAPI() {
-    override var mainUrl = "https://yomovies.ing"
+    override var mainUrl = "https://yomovies.diy/"
     private var directUrl = ""
     override var name = "Yomovies"
     override val hasMainPage = true
