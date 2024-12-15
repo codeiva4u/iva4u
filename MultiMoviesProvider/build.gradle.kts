c2933a87e9f87d4eb46521509a0100d8f36e87e2
codeiva4u <153196182+codeiva4u@users.noreply.github.com>
2024-12-15 22:08:21 +0530

    Update build.gradle.kts

--- MultiMoviesProvider/build.gradle.kts
+++ MultiMoviesProvider/build.gradle.kts
@@ -1,6 +1,6 @@
 import org.jetbrains.kotlin.konan.properties.Properties
 // use an integer for version numbers
-version = 29
+version = 30
 
 cloudstream {
     // All of these properties are optional, you can safely remove them
