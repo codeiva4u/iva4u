e53fde1abc82448f001a9ae14ecc5b0a8ea7dc84
codeiva4u <153196182+codeiva4u@users.noreply.github.com>
2024-12-25 22:53:28 +0530

    Update build.gradle.kts

--- TheMoviesFlix/build.gradle.kts
+++ TheMoviesFlix/build.gradle.kts
@@ -1,5 +1,5 @@
 // use an integer for version numbers
-version = 3
+version = 4
 
 cloudstream {
     description = "720p Movies, 1080p movies, Dual Audio Movies, Hindi Dubbed Series, Hollywood Movies"
