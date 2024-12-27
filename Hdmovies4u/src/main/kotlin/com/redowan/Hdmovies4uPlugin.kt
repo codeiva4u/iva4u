733bc52331156d2b4171b6b77e5f662da6185f05
codeiva4u <codeiva@protonmail.com>
2024-12-27 19:05:23 +0530

    Merge remote-tracking branch 'origin/master'

--- Hdmovies4u/src/main/kotlin/com/redowan/Hdmovies4uPlugin.kt
+++ Hdmovies4u/src/main/kotlin/com/redowan/Hdmovies4uPlugin.kt
@@ -3,16 +3,14 @@ package com.redowan
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
-import com.lagradost.cloudstream3.extractors.Voe
-import com.lagradost.cloudstream3.extractors.StreamWishExtractor
 
+@Suppress("unused")
 @CloudstreamPlugin
-class Hdmovies4uPlugin: Plugin() {
+class Hdmovies4uPlugin : Plugin() {
     override fun load(context: Context) {
         // All providers should be added in this manner. Please don't edit the providers list directly.
         registerMainAPI(Hdmovies4u())
         registerExtractorAPI(Voe())
-        registerExtractorAPI(StreamWishExtractor())
         registerExtractorAPI(DoodStream())
         registerExtractorAPI(StreamWish())
         registerExtractorAPI(Drivetot())
