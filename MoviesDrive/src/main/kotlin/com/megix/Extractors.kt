5ad1ed5fedf904b2e8a27e311cebab0de0f763e0
codeiva4u <codeiva@protonmail.com>
2025-01-23 21:05:17 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	MoviesDrive/src/main/kotlin/com/megix/Extractors.kt

--- MoviesDrive/src/main/kotlin/com/megix/Extractors.kt
+++ MoviesDrive/src/main/kotlin/com/megix/Extractors.kt
@@ -49,60 +49,60 @@ open class HubCloud : ExtractorApi() {
         subtitleCallback: (SubtitleFile) -> Unit,
         callback: (ExtractorLink) -> Unit
     ) {
-        // 1. Pixeldrain लिंक्स को हैंडल करें
+        // 1. Pixeldrain लिंक्स को हैंडल करें (बिना return के)
         val fileId = pixeldrainRegex.find(url)?.groupValues?.get(1)
         if (!fileId.isNullOrEmpty()) {
             callback.invoke(
                 ExtractorLink(
                     "PixelDrain",
                     "PixelDrain",
-                    "https://pixeldra.in/api/file/$fileId?download",
+                    "https://pixeldrain.com/api/file/$fileId?download", // डोमेन सही करें (pixeldrain.com)
                     url,
                     Qualities.Unknown.value
                 )
             )
-            return
+            // return नहीं लगाएं ताकि FSL लिंक्स भी प्रोसेस हों
         }
 
         // 2. FSL Server लिंक्स निकालें
         try {
             val doc = app.get(url, headers = mapOf("Referer" to mainUrl)).document
-            doc.select("h2 > a.btn.btn-success.btn-lg.h6").forEach { link ->
-                if (link.text().contains("FSL Server", ignoreCase = true)) {
-                    // URL सैनिटाइज़ करें
-                    val href = link.attr("href")
-                        .replace("[[ moviesdrives.com ]]", "moviesdrives.com")
-                        .replace(" ", "%20") // स्पेस को एन्कोड करें
 
-                    // क्वालिटी डिटेक्ट करें
-                    val qualityMatch = fslQualityRegex.find(link.text())
-                    val (qualityName, qualityValue) = when {
-                        qualityMatch != null -> {
-                            val q = qualityMatch.value
-                            Pair(q, when (q.lowercase()) {
-                                "720p" -> Qualities.P720.value
-                                "1080p" -> Qualities.P1080.value
-                                "4k" -> Qualities.P2160.value
-                                else -> Qualities.Unknown.value
-                            })
+            // Pixeldrain के अलावा अन्य लिंक्स के लिए
+            if (fileId == null) {
+                doc.select("h2 > a.btn.btn-success.btn-lg.h6").forEach { link ->
+                    if (link.text().contains("FSL Server", ignoreCase = true)) {
+                        val href = link.attr("href")
+                            .replace("[[ moviesdrives.com ]]", "moviesdrives.com")
+                            .replace(" ", "%20")
+
+                        val qualityMatch = fslQualityRegex.find(link.text())
+                        val (qualityName, qualityValue) = when {
+                            qualityMatch != null -> {
+                                val q = qualityMatch.value
+                                Pair(q, when (q.lowercase()) {
+                                    "720p" -> Qualities.P720.value
+                                    "1080p" -> Qualities.P1080.value
+                                    "4k" -> Qualities.P2160.value
+                                    else -> Qualities.Unknown.value
+                                })
+                            }
+                            else -> Pair("Unknown", Qualities.Unknown.value)
                         }
-                        else -> Pair("Unknown", Qualities.Unknown.value)
-                    }
 
-                    // लिंक को कॉलबैक में पास करें
-                    callback.invoke(
-                        ExtractorLink(
-                            "FSL Server",
-                            "FSL $qualityName",
-                            href,
-                            url,
-                            qualityValue
+                        callback.invoke(
+                            ExtractorLink(
+                                "FSL Server",
+                                "FSL $qualityName",
+                                href,
+                                url,
+                                qualityValue
+                            )
                         )
-                    )
+                    }
                 }
             }
         } catch (e: Exception) {
-            // एरर हैंडलिंग (लॉग करें या इग्नोर करें)
             e.printStackTrace()
         }
     }
