4127e52a2639d93b2f42b877b5d1dfb3dedd2470
codeiva4u <codeiva@protonmail.com>
2025-04-15 23:53:29 +0530

    Update

--- Movierulzhd/src/main/kotlin/com/phisher98/MovierulzhdPlugin.kt
+++ Movierulzhd/src/main/kotlin/com/phisher98/MovierulzhdPlugin.kt
@@ -1,6 +1,5 @@
 package com.phisher98
 
-// Import new extractors
 import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 
@@ -9,12 +8,10 @@ class MovierulzhdPlugin: BasePlugin() {
     override fun load() {
         // All providers should be added in this manner. Please don't edit the providers list directly.
         registerMainAPI(Movierulzhd())
-        // registerExtractorAPI(FilemoonV2()) // Commented out as FilemoonV2 is not defined
-        // New extractors for 1movierulzhd.lol
-        registerExtractorAPI(MovieRulzVidstack())
-        registerExtractorAPI(MovieRulzStreamcasthub())
-        registerExtractorAPI(MovieRulzFilemoon())
-        registerExtractorAPI(MovieRulzAkamaicdn())
-        registerExtractorAPI(MovieRulzMocdn())
+        
+        // Register custom extractors
+        registerExtractorAPI(Daddylive())
+        registerExtractorAPI(DeadDrive())
+        registerExtractorAPI(GdMirrorBot())
     }
 }
--- MultiMoviesProvider/src/main/kotlin/com/phisher98/MultiMoviesProviderPlugin.kt
+++ MultiMoviesProvider/src/main/kotlin/com/phisher98/MultiMoviesProviderPlugin.kt
@@ -1,7 +1,5 @@
 package com.phisher98
 
-import com.lagradost.cloudstream3.extractors.MixDrop
-import com.lagradost.cloudstream3.extractors.VidHidePro5
 import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 
@@ -10,12 +8,11 @@ class MultiMoviesProviderPlugin: BasePlugin() {
     override fun load() {
         // All providers should be added in this manner. Please don't edit the providers list directly.
         registerMainAPI(MultiMoviesProvider())
-        registerExtractorAPI(VidHidePro5())
-        registerExtractorAPI(MixDrop())
-        // New extractors for multimovies.guru
-        registerExtractorAPI(MultiMoviesGuruVidstack())
-        registerExtractorAPI(MultiMoviesGuruStreamcasthub())
-        registerExtractorAPI(MultiMoviesGuruFilemoon())
-        registerExtractorAPI(MultiMoviesGuruGDMirrorbot())
-        }
+        
+        // Register custom extractors
+        registerExtractorAPI(MultiGdMirrorBot())
+        registerExtractorAPI(MultiDeadDrive())
+        registerExtractorAPI(MultiStreamSB())
+        registerExtractorAPI(MultiDirectLink())
+    }
 }
