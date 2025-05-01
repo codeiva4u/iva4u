dde83c38169b2994e50a00f4e211cd707086c85f
codeiva4u <153196182+codeiva4u@users.noreply.github.com>
2025-05-01 19:15:10 +0530

    Update HDhub4uProvider.kt

--- HDhub4u/src/main/kotlin/com/hdhub4u/HDhub4uProvider.kt
+++ HDhub4u/src/main/kotlin/com/hdhub4u/HDhub4uProvider.kt
@@ -28,7 +28,7 @@ import org.jsoup.select.Elements
 
 
 class HDhub4uProvider : MainAPI() {
-    override var mainUrl = "https://hdhub4u.mn/"
+    override var mainUrl = "https://hdhub4u.graphics/"
     override var name = "HDHub4U"
     override var lang = "hi"
     override val hasMainPage = true
