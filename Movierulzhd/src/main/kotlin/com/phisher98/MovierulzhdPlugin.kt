0ed9c986d6215989ec27a83d45db8800d2cb8798
unknown <codeiva@protonmail.com>
2025-07-05 00:25:52 +0530

    first commit

--- Movierulzhd/src/main/kotlin/com/phisher98/MovierulzhdPlugin.kt
+++ Movierulzhd/src/main/kotlin/com/phisher98/MovierulzhdPlugin.kt
@@ -2,16 +2,23 @@ package com.phisher98
 
 import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.extractors.VidSrcTo
 
 @CloudstreamPlugin
 class MovierulzhdPlugin: BasePlugin() {
     override fun load() {
         // All providers should be added in this manner. Please don't edit the providers list directly.
         registerMainAPI(Movierulzhd())
+    //    registerMainAPI(Hdmovie2())
+        registerExtractorAPI(FMHD())
         registerExtractorAPI(VidSrcTo())
+        registerExtractorAPI(Server1uns())
         registerExtractorAPI(Akamaicdn())
+        registerExtractorAPI(Luluvdo())
         registerExtractorAPI(FMX())
-        registerExtractorAPI(FilemoonV2())
+        registerExtractorAPI(Lulust())
+        registerExtractorAPI(Playonion())
+        registerExtractorAPI(Movierulz())
+        registerExtractorAPI(GDFlix())
+        
     }
 }
