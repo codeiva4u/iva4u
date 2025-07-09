d9da68123c200e5c07f75ac7c506d3ad22caef0b
codeiva4u <codeiva@protonmail.com>
2025-07-09 22:59:10 +0530

    Update Provider

--- MultiMoviesProvider/src/main/kotlin/com/phisher98/MultiMoviesProviderPlugin.kt
+++ MultiMoviesProvider/src/main/kotlin/com/phisher98/MultiMoviesProviderPlugin.kt
@@ -3,22 +3,41 @@ package com.phisher98
 import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 
-
 @CloudstreamPlugin
 class MultiMoviesProviderPlugin: BasePlugin() {
     override fun load() {
         // All providers should be added in this manner. Please don't edit the providers list directly.
         registerMainAPI(MultiMoviesProvider())
-        registerExtractorAPI(Multimovies())
-        registerExtractorAPI(Animezia())
-        registerExtractorAPI(server2())
-        registerExtractorAPI(MultimoviesAIO())
-        registerExtractorAPI(GDMirrorbot())
-        registerExtractorAPI(Asnwish())
-        registerExtractorAPI(CdnwishCom())
-        registerExtractorAPI(Strwishcom())
-        registerExtractorAPI(Streamcasthub())
+        registerExtractorAPI(VidSrcTo())
+        registerExtractorAPI(Server1uns())
+        registerExtractorAPI(Akamaicdn())
+        registerExtractorAPI(Luluvdo())
+        registerExtractorAPI(FMX())
+        registerExtractorAPI(Lulust())
+        registerExtractorAPI(VidHidePro())
+        registerExtractorAPI(VidHidePro1())
         registerExtractorAPI(Dhcplay())
-        registerExtractorAPI(server1())
-        }
+        registerExtractorAPI(VidHidePro2())
+        registerExtractorAPI(VidHidePro3())
+        registerExtractorAPI(VidHidePro4())
+        registerExtractorAPI(VidHidePro5())
+        registerExtractorAPI(VidHidePro6())
+        registerExtractorAPI(Smoothpre())
+        registerExtractorAPI(Dhtpre())
+        registerExtractorAPI(VidStack())
+        registerExtractorAPI(Movierulz())
+        registerExtractorAPI(Filesim())
+        registerExtractorAPI(FileMoonSx())
+        registerExtractorAPI(Ztreamhub())
+        registerExtractorAPI(Movhide())
+        registerExtractorAPI(StreamhideCom())
+        registerExtractorAPI(StreamhideTo())
+        registerExtractorAPI(FileMoonIn())
+        registerExtractorAPI(Peytonepre())
+        registerExtractorAPI(Moviesm4u())
+        registerExtractorAPI(Ahvsh())
+        registerExtractorAPI(Guccihide())
+        registerExtractorAPI(GDFlix())
+
+    }
 }
