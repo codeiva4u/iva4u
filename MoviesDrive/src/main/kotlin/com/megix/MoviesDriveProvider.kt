70f2a5c94029874e2d1a8be93f7e10e513d2c9e6
codeiva4u <153196182+codeiva4u@users.noreply.github.com>
2025-07-24 21:32:24 +0530

    Update MoviesDriveProvider.kt

--- MoviesDrive/src/main/kotlin/com/megix/MoviesDriveProvider.kt
+++ MoviesDrive/src/main/kotlin/com/megix/MoviesDriveProvider.kt
@@ -27,7 +27,7 @@ import com.lagradost.cloudstream3.utils.loadExtractor
 import org.jsoup.nodes.Element
 
 class MoviesDriveProvider : MainAPI() { // all providers must be an instance of MainAPI
-    override var mainUrl = "https://moviesdrive.click"
+    override var mainUrl = "https://moviesdrive.zip"
     override var name = "MoviesDrive"
     override val hasMainPage = true
     override var lang = "hi"
