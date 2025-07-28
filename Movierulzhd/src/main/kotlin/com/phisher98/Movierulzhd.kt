864d407b5d8b9173abda4b815caa5e8f539cd636
codeiva4u <153196182+codeiva4u@users.noreply.github.com>
2025-07-28 16:07:11 +0530

    Update Movierulzhd.kt

--- Movierulzhd/src/main/kotlin/com/phisher98/Movierulzhd.kt
+++ Movierulzhd/src/main/kotlin/com/phisher98/Movierulzhd.kt
@@ -17,7 +17,7 @@ import java.net.URI
 
 open class Movierulzhd : MainAPI() {
 
-    override var mainUrl = "https://1movierulzhd.art/"
+    override var mainUrl = "https://1movierulzhd.wtf/"
     var directUrl = ""
     override var name = "Movierulzhd"
     override val hasMainPage = true
