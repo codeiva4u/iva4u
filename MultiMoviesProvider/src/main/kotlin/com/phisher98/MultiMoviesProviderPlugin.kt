1ac135bdc3904873b4d29d8e49b6957e64ea5f04
codeiva4u <codeiva4u@gmail.com>
2025-09-30 22:19:57 +0530

    Update Provider

--- MultiMoviesProvider/src/main/kotlin/com/phisher98/MultiMoviesProviderPlugin.kt
+++ MultiMoviesProvider/src/main/kotlin/com/phisher98/MultiMoviesProviderPlugin.kt
@@ -2,7 +2,7 @@ package com.phisher98
 
 import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.extractors.VidHidePro
+
 
 
 @CloudstreamPlugin
@@ -10,18 +10,6 @@ class MultiMoviesProviderPlugin: BasePlugin() {
     override fun load() {
         // All providers should be added in this manner. Please don't edit the providers list directly.
         registerMainAPI(MultiMoviesProvider())
-        registerExtractorAPI(Multimovies())
-        registerExtractorAPI(Animezia())
-        registerExtractorAPI(server2())
-        registerExtractorAPI(MultimoviesAIO())
-        registerExtractorAPI(GDMirrorbot())
-        registerExtractorAPI(Asnwish())
-        registerExtractorAPI(CdnwishCom())
-        registerExtractorAPI(Strwishcom())
-        registerExtractorAPI(Streamcasthub())
-        registerExtractorAPI(Dhcplay())
-        registerExtractorAPI(server1())
-        registerExtractorAPI(VidHidePro())
         // Download Links Extractors
         registerExtractorAPI(GDTOTExtractor())
         registerExtractorAPI(FilePress ())
