b7b717d267aa768796d3efc3f34b0dcda5b1bfcd
codeiva4u <153196182+codeiva4u@users.noreply.github.com>
2025-11-19 21:16:14 +0530

    Update Movierulzhd.kt

--- Movierulzhd/src/main/kotlin/com/phisher98/Movierulzhd.kt
+++ Movierulzhd/src/main/kotlin/com/phisher98/Movierulzhd.kt
@@ -35,7 +35,7 @@ import java.net.URI
 
 open class Movierulzhd : MainAPI() {
 
-    override var mainUrl = "https://123multihub.one/"
+    override var mainUrl = "https://2filmyzilla.sbs/"
     var directUrl = ""
     override var name = "Movierulzhd"
     override val hasMainPage = true
