fc514993212e539f48e8ea96dc4b2462c0a0b428
Admin <admin@iva4u>
2026-05-02 19:05:41 +0530

    Remove expired hardcoded cookie causing Retry Connection error

--- HDhub4u/src/main/kotlin/com/hdhub4u/HDhub4uProvider.kt
+++ HDhub4u/src/main/kotlin/com/hdhub4u/HDhub4uProvider.kt
@@ -124,7 +124,7 @@ class HDhub4uProvider : MainAPI() {
         "category/south-hindi-movies/" to "South Hindi Dubbed",
         "category/web-series/" to "Web Series",
     )
-    private val headers = mapOf("User-Agent" to "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0","Cookie" to "xla=s4t")
+    private val headers = mapOf("User-Agent" to "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0")
 
     override suspend fun getMainPage(
         page: Int,
