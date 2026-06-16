788724e7ae2d07d63ded4c4fff39265b84d23359
codeiva4u <admin@example.com>
2026-06-16 20:50:51 +0530

    Clean up debugging scripts

--- test-doc.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const html = fs.readFileSync('html_dump_2.txt', 'utf8');
-const cheerio = require('cheerio');
-const $ = cheerio.load(html);
-
-console.log($('ul#playeroptionsul li').length);
